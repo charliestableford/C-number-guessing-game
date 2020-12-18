17dc29abfc2a717eee351850c674b5e396037ea4
Charlie Stableford <charliestableford2@gmail.com>
2020-12-18 14:15:50 -0500

    simple game no bells and whistles

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -7,6 +7,36 @@ namespace ConsoleApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hey there, welcome to my guessing game");
+
+            // make a random number between 0-100
+            var RandomNum = new Random().Next(101);
+            Console.WriteLine(RandomNum);
+            var guess = -1;
+
+            while(guess != RandomNum)
+            {
+                Console.WriteLine("Take a guess at what I am thinking, a number between 1-100.");
+
+                var input = Console.ReadLine();
+            
+
+            if(int.TryParse(input, out guess))
+            {
+                if(guess == RandomNum)
+                {
+                    Console.WriteLine($"Woah you are a mind reader. I was thinking {RandomNum}.");
+                    break;
+                }
+                else 
+                {
+                    Console.WriteLine($"Not quite. I think you should guess {(guess > RandomNum? "lower." : "higher.")}");
+                }
+                }
+                
+            }
+            //Run after loop
+            Console.WriteLine("Thanks for playing");
+           
         }
     }
 }
